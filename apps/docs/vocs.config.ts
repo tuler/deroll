07aa8b7a38855fbf7f4780b856cfc6f3703a2216
Danilo Tuler <tuler@pobox.com>
2024-08-20 15:54:44 -0300

    Reference docs placeholder

--- apps/docs/vocs.config.ts
+++ apps/docs/vocs.config.ts
@@ -76,51 +76,67 @@ export default defineConfig({
                     items: [
                         {
                             text: "createWallet",
+                            link: "/wallet/create-wallet",
                         },
                         {
                             text: "getWallet",
+                            link: "/wallet/get-wallet",
                         },
                         {
                             text: "etherBalanceOf",
+                            link: "/wallet/ether-balance-of",
                         },
                         {
                             text: "erc20BalanceOf",
+                            link: "/wallet/erc20-balance-of",
                         },
                         {
                             text: "erc721Has",
+                            link: "/wallet/erc721-has",
                         },
                         {
                             text: "erc1155BalanceOf",
+                            link: "/wallet/erc1155-balance-of",
                         },
                         {
                             text: "transferEther",
+                            link: "/wallet/transfer-ether",
                         },
                         {
                             text: "transferERC20",
+                            link: "/wallet/transfer-erc20",
                         },
                         {
                             text: "transferERC721",
+                            link: "/wallet/transfer-erc721",
                         },
                         {
                             text: "transferERC1155",
+                            link: "/wallet/transfer-erc1155",
                         },
                         {
                             text: "transferBatchERC1155",
+                            link: "/wallet/transfer-batch-erc1155",
                         },
                         {
                             text: "withdrawEther",
+                            link: "/wallet/withdraw-ether",
                         },
                         {
                             text: "withdrawERC20",
+                            link: "/wallet/withdraw-erc20",
                         },
                         {
                             text: "withdrawERC721",
+                            link: "/wallet/withdraw-erc721",
                         },
                         {
                             text: "withdrawERC1155",
+                            link: "/wallet/withdraw-erc1155",
                         },
                         {
                             text: "withdrawBatchERC1155",
+                            link: "/wallet/withdraw-batch-erc1155",
                         },
                     ],
                 },
@@ -131,9 +147,11 @@ export default defineConfig({
                     items: [
                         {
                             text: "createRouter",
+                            link: "/router/create-router",
                         },
                         {
                             text: "add",
+                            link: "/router/add",
                         },
                     ],
                 },
