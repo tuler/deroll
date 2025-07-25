650db57850ed6ecd35624698dd5d4b2682bce9e6
Danilo Tuler <tuler@pobox.com>
2025-07-25 14:05:35 -0400

    Deprecate @deroll/rpc in favor of @cartesi/rpc

--- packages/rpc/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { createClient } from "./client";
-export { createServer } from "./server";
-export * from "./types";
--- packages/rpc/tsup.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-    entry: ["src/cli.ts", "src/client.ts", "src/index.ts", "src/server.ts"],
-    format: ["cjs", "esm"],
-    dts: true,
-});
